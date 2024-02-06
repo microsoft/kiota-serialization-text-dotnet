0ed65df9278f77c829ef80f78d580d2bd92b8c3a
David Pine <david.pine@microsoft.com>
2024-02-06 08:05:12 -0600

    Update TextParseNode.cs
    I believe that this is incorrect, and should be `PublicFields`. Related to https://github.com/microsoft/kiota-serialization-text-dotnet/pull/140/files#r1477315267

--- src/TextParseNode.cs
+++ src/TextParseNode.cs
@@ -67,14 +67,14 @@ public class TextParseNode : IParseNode
     public Time? GetTimeValue() => DateTime.TryParse(Text, out var result) ? new Time(result) : null;
     /// <inheritdoc />
 #if NET5_0_OR_GREATER
-    public IEnumerable<T?> GetCollectionOfEnumValues<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>() where T : struct, Enum
+    public IEnumerable<T?> GetCollectionOfEnumValues<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields)] T>() where T : struct, Enum
 #else
     public IEnumerable<T?> GetCollectionOfEnumValues<T>() where T : struct, Enum
 #endif
     => throw new InvalidOperationException(NoStructuredDataMessage);
     /// <inheritdoc />
 #if NET5_0_OR_GREATER
-    public T? GetEnumValue<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.All)] T>() where T : struct, Enum
+    public T? GetEnumValue<[DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicFields)] T>() where T : struct, Enum
 #else
     public T? GetEnumValue<T>() where T : struct, Enum
 #endif
