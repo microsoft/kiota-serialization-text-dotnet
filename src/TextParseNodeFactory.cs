305b695dbf817cfc7316d71047a9cce2a8435bc5
Miha Markič <miha@rthand.com>
2024-05-04 11:55:53 +0200

    Implements IAsyncParseNodeFactory interface

--- src/TextParseNodeFactory.cs
+++ src/TextParseNodeFactory.cs
@@ -1,13 +1,15 @@
 using System;
 using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Kiota.Abstractions.Serialization;
 
 namespace Microsoft.Kiota.Serialization.Text;
 
 /// <summary>
-/// The <see cref="IParseNodeFactory"/> implementation for text/plain content types
+/// The <see cref="IAsyncParseNodeFactory"/> implementation for text/plain content types
 /// </summary>
-public class TextParseNodeFactory : IParseNodeFactory
+public class TextParseNodeFactory : IAsyncParseNodeFactory
 {
     /// <inheritdoc />
     public string ValidContentType => "text/plain";
@@ -21,6 +23,20 @@ public class TextParseNodeFactory : IParseNodeFactory
         _ = content ?? throw new ArgumentNullException(nameof(content));
         using var reader = new StreamReader(content);
         var stringContent = reader.ReadToEnd();
+        return new TextParseNode(stringContent);
+    }
+    /// <inheritdoc />
+    public async Task<IParseNode> GetRootParseNodeAsync(string contentType, Stream content, CancellationToken cancellationToken = default)
+    {
+        if(string.IsNullOrEmpty(contentType))
+            throw new ArgumentNullException(nameof(contentType));
+        else if(!ValidContentType.Equals(contentType, StringComparison.OrdinalIgnoreCase))
+            throw new ArgumentOutOfRangeException($"expected a {ValidContentType} content type");
+
+        _ = content ?? throw new ArgumentNullException(nameof(content));
+
+        using var reader = new StreamReader(content);
+        var stringContent = await reader.ReadToEndAsync();
         return new TextParseNode(stringContent);
     }
 }
